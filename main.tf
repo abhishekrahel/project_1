7324b726288b49c15260c274bb9e5ccd4a6cd913
arahel <abhishekrahel@example.com>
2026-06-08 19:27:35 +0530

    env4

--- main.tf
+++ main.tf
@@ -2,11 +2,16 @@ provider "aws"{
     region = "us-east-1"
 }
 module "server" {
- source = "./sample_module_pipeline"
+   source = "./sample_module_pipeline"
 
    ami = var.ami
    instance_type = var.instance_type
    tags = var.tags
    region = var.region
+   environment = var.environment
+   
 
 }
+
+
+
